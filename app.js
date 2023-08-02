801927dcc26beaa66e3989ad7cb14f841f740c95
Trupti Lakhara <107105068+truptilakhara@users.noreply.github.com>
2023-08-02 13:44:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const passportLocal = require("passport-local");
 const User = require("./models/user");
 const mongoSanitize = require("express-mongo-sanitize");
 const password = encodeURIComponent("8dzcDid9B0HEqbBh");
-const db_url = `mongodb+srv://truptilakhara:${password}@cluster0.cixwxze.mongodb.net/?retryWrites=true&w=majority`;
+const db_url = `mongodb+srv://truptilakhara:${password}@cluster0.cixwxze.mongodb.net/?retryWrites=true&w=majority` || 'mongodb://localhost:27017/yelp-camp'
 const MongoStore = require("connect-mongo");
 const store = MongoStore.create({
   mongoUrl: db_url,
